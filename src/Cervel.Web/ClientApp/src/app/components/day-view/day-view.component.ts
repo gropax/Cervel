5a49740fd3ad696b47bba9b781f939457585c52a
Maxime Laudrin <maxime.laudrin@protonmail.com>
2022-04-30 20:08:43 +0200

    Animate selection changes.

--- src/Cervel.Web/ClientApp/src/app/components/day-view/day-view.component.ts
+++ src/Cervel.Web/ClientApp/src/app/components/day-view/day-view.component.ts
@@ -1,20 +1,40 @@
-import { Input } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Highlight } from '../../models/highlights';
 
 @Component({
   selector: 'app-day-view',
   templateUrl: './day-view.component.html',
-  styleUrls: ['./day-view.component.less']
+  styleUrls: ['./day-view.component.less'],
+  animations: [
+    trigger('changeState', [
+      state('active', style({
+        backgroundColor: '#7b1fa2',
+      })),
+      state('inactive', style({
+        backgroundColor: '#747474'
+      })),
+      transition('* => active', animate('200ms')),
+      transition('* => inactive', animate('200ms'))
+    ])
+  ]
 })
-export class DayViewComponent implements OnInit {
+export class DayViewComponent implements OnInit, OnChanges {
 
   @Input() year!: number;
   @Input() month!: number;
   @Input() day!: number;
   @Input() highlights: Highlight[] = [];
 
+  public state: 'active' | 'inactive' = 'inactive';
+
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    var highlights = changes['highlights'].currentValue;
+    this.state = highlights.length > 0 ? 'active' : 'inactive';
+  }
 
   ngOnInit(): void {
   }
