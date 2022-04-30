2a1639cc8c1dbed182f844041a5e961ac039acc3
Maxime Laudrin <maxime.laudrin@protonmail.com>
2022-04-30 13:32:36 +0200

    Display correct day layout in month views.

--- src/Cervel.Web/ClientApp/src/app/components/month-view/month-view.component.ts
+++ src/Cervel.Web/ClientApp/src/app/components/month-view/month-view.component.ts
@@ -35,4 +35,44 @@ export class MonthViewComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public dayRange() {
+    var days = [];
+    var dayNb = new Date(this.year, this.month, 0).getDate();
+    var weekNb = 1;
+
+    for (var i = 1; i <= dayNb; i++) {
+      var date = new Date(this.year, this.month - 1, i);
+      var dow = (date.getDay() + 6) % 7 + 1;
+      if (dow == 1 && i != 1)
+        weekNb++;
+
+      days.push(new DayInfo(this.year, this.month, i, dow, weekNb));
+    }
+
+    console.log(`Month: ${this.month}`);
+    console.log(`DayNb: ${dayNb}`);
+    console.log(days);
+    return days;
+  }
+
+  private dayOfWeekClasses = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+  ]
+
+}
+
+export class DayInfo {
+  constructor(
+    public year: number,
+    public month: number,
+    public day: number,
+    public dayOfWeek: number,
+    public weekNumber: number) {
+  }
 }
